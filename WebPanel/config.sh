37de95097d2bf357ba8fd05ce4668dd4f7b6d808
Siwa12100 <jean.marcillac12@gmail.com>
2025-01-30 08:15:55 +0000

    Ajout scripts et dockerfile pour déploiement

---
+++ WebPanel/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Configuration du projet
+IMAGE_NAME="projet-ia-panel-web:latest"
+CONTAINER_NAME="projet-ia-panel-web"
+PORT="21006"
+
+export IMAGE_NAME
+export CONTAINER_NAME
+export PORT
