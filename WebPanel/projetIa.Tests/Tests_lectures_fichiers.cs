d479d0908402fb14b500d6d6b9e08301c08c2e41
Siwa12100 <jean.marcillac12@gmail.com>
2025-01-30 08:38:21 +0000

    Ajout de tests unitaires et configuration de projet pour les tests

---
+++ WebPanel/projetIa.Tests/Tests_lectures_fichiers.cs
@@ -0,0 +1,26 @@
+﻿using projetIa.Service;
+using projetIa.WebClient;
+
+namespace projetIa.Tests;
+
+public class Tests_lectures_fichiers
+{
+
+    protected IService? service;
+    protected IWebClient? webClient;
+
+    public Tests_lectures_fichiers()
+    {
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri("http://localhost:5000/api/");
+        this.webClient = new WebClient.WebClient(client);
+        this.service = new Service.Service(webClient);
+    }
+    
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(4, 2 + 2);
+    }
+}
