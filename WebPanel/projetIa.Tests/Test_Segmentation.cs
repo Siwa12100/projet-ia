df8f795961a8c8225ddb2e99493b014bb90288c2
Siwa12100 <jean.marcillac12@gmail.com>
2025-02-02 15:46:21 +0000

    Ajout de tests pour la segmentation et la classification des images dans Test_Segmentation.cs

--- WebPanel/projetIa.Tests/Test_Segmentation.cs
+++ WebPanel/projetIa.Tests/Test_Segmentation.cs
@@ -7,6 +7,7 @@ namespace projetIa.Tests
     {
         protected IService? service;
         protected IWebClient? webClient;
+        protected readonly RecuperateurImages recuperateurImages;
 
         public Test_Segmentation()
         {
@@ -14,6 +15,39 @@ namespace projetIa.Tests
             client.BaseAddress = new Uri("http://149.7.5.30:21089/api/");
             this.webClient = new WebClient.WebClient(client);
             this.service = new Service.Service(webClient);
+            this.recuperateurImages = new RecuperateurImages();
+        }
+
+        [Fact]
+        public async Task Test_SegmentationImage()
+        {
+            var nomImageAEnvoyer = "image1.jpg";
+            var image = recuperateurImages.RecupererImage(null, nomImageAEnvoyer);
+            Assert.NotNull(image);
+
+            var ImageByteArray = File.ReadAllBytes(image.FullName);
+            Assert.NotNull(ImageByteArray);
+            Assert.NotNull(webClient);
+            var zipArchive = await webClient.SegmenterImageAsync(ImageByteArray);
+            Assert.NotNull(zipArchive);
+        }
+
+        [Fact]
+        public async Task Test_ClassificationGenre()
+        {
+            var nomImageAEnvoyer = "1.jpg";
+            var nomDossierImage = "CropsManuels";
+            var image = recuperateurImages.RecupererImage(nomDossierImage, nomImageAEnvoyer);
+            Assert.NotNull(image);
+
+            var ImageByteArray = File.ReadAllBytes(image.FullName);
+            Assert.NotNull(ImageByteArray);
+            Assert.NotNull(webClient);
+
+            var resultat = await webClient.ClassifierParGenreAsync(ImageByteArray);
+            Assert.NotNull(resultat);
+
+            Assert.Equal("male", resultat.gender);
         }
     }
 }
